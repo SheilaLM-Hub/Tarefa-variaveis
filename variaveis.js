a5de0c2eb713389da6cbbbc08e4b6f99fbfeb27f
SheilaLM-Hub <91496863+SheilaLM-Hub@users.noreply.github.com>
2023-06-29 21:15:41 -0300

    Entrega tarefa

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+var carregamento = "Página carregada com sucesso!";
+alert(carregamento);
+
+var quiloLaranja= 5.00;
+var quiloLimao= 3.00;
+
+
+
+var kg= quiloLaranja+quiloLimao;
+document.getElementById("soma").innerHTML = kg;
+
+var kg= quiloLaranja/2;
+document.getElementById("divisão").innerHTML = kg;
+
+var kg=  quiloLimao*3;
+document.getElementById("multiplicação").innerHTML = kg;
+
+var kg= quiloLaranja==quiloLimao;
+document.getElementById("comparação").innerHTML = kg;
+
